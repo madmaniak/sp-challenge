447461a1317dd36a2519d22dbe3f8c6782197445
Marek Piasecki <marek.piasecki@netguru.pl>
2013-12-15 16:01:14 +0100

    display offers from api

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,11 @@
 class OffersController < ApplicationController
+
+  expose(:offers) { OffersDecorator.new Offers.new( offers_params ).get }
+
+  private
+
+  def offers_params
+    params.permit :uid, :pub0, :page
+  end
+
 end
