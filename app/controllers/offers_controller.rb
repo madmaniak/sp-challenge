6e542dd3eb4eb83ab52da5f7fb78c76b8da60990
Marek Piasecki <marek.piasecki@netguru.pl>
2013-12-15 16:06:58 +0100

    display alerts from offers message

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,10 +2,19 @@ class OffersController < ApplicationController
 
   expose(:offers) { OffersDecorator.new Offers.new( offers_params ).get }
 
+  before_filter :set_flash, only: :index
+
   private
 
   def offers_params
     params.permit :uid, :pub0, :page
+  end
+  
+  def set_flash
+    if offers.message.present?
+      type = offers.fine? ? :success : :alert
+      flash.now[type] = offers.message
+    end
   end
 
 end
